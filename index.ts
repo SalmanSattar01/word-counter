6b1c0b2317ad72dd4abc02b54c417e3bcb7005ba
salman sattar <salmansattar01@gmail.com>
2024-04-20 17:17:07 +0500

    code added

--- index.ts
+++ index.ts
@@ -2,3 +2,24 @@
 
 import inquirer from "inquirer";
 
+const answers:{
+    Sentence:string
+}  = await inquirer.prompt(
+    [
+        {
+            name:"Sentence",
+            type:"input",
+            message:"Enter your sentence to count the words:"
+        }
+
+    ])
+
+    const words = answers.Sentence.trim().split(" ")
+
+    // Print the array of words to the console
+
+    console.log(words)
+
+    //Print the word count of the sentence of the console
+
+    console.log(`Your sentence word count is ${words.length}`)
